b1071044bfd46fc1952c4dbd0b00e292cfbbeab9
Ho Hang Au Yeung <mayeung@telus.net>
2019-08-28 18:29:04 -0700

    add language drop down

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import "./TopBar.css";
 import { withRouter } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 function TopBar({ location }) {
   const { pathname } = location;
+  const { t, i18n } = useTranslation();
+  const changeLanguage = lng => {
+    i18n.changeLanguage(lng);
+  };
 
   return (
     <Navbar bg="primary" expand="lg" variant="dark">
-      <Navbar.Brand href="#home">New York Times App</Navbar.Brand>
+      <Navbar.Brand href="#home">{t("New York Times App")}</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
@@ -19,6 +25,14 @@ function TopBar({ location }) {
           <Nav.Link href="/search" active={pathname.includes("/search")}>
             Search
           </Nav.Link>
+          <NavDropdown title="Language" id="basic-nav-dropdown">
+            <NavDropdown.Item onClick={() => changeLanguage("en")}>
+              English
+            </NavDropdown.Item>
+            <NavDropdown.Item onClick={() => changeLanguage("fr")}>
+              French
+            </NavDropdown.Item>
+          </NavDropdown>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
---
+++ src/translations.js
@@ -0,0 +1,14 @@
+const resources = {
+  en: {
+    translation: {
+      "New York Times App": "New York Times App",
+    },
+  },
+  fr: {
+    translation: {
+      "New York Times App": "App New York Times",
+    },
+  },
+};
+
+export { resources };
